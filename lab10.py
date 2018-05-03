f39df4610b3306d77b1e42e5c71eb77dce8a51bf
Abu Butt <butabu@yahoo.com>
2018-05-03 15:22:27 -0400

    Modify

--- lab10.py
+++ lab10.py
@@ -1,62 +1,49 @@
-
 from pyspark import SparkContext
 
-if __name__ == '__main__':
-	sc = SparkContext()
+def filterBike(pId, lines):
+    import csv
+    for row in csv.reader(lines):
+        if (row[6] == 'Greenwich Ave & 8 Ave' and row[3].startswith('2015-02-01')):
+            yield (row[3][:19])
 
-	taxi = sc.textFile('yellow.csv.gz')
-	bike = sc.textFile('citibike.csv')
-	list(enumerate(bike.take(1)[0].split(',')))
-	
-	def filterBike(pId, lines):
-	    import csv
-	    for row in csv.reader(lines):
-		if (row[6]=='Greenwich Ave & 8 Ave' and 
-		    row[3].startswith('2015-02-01')):
-		    yield (row[3][:19])		    
-	gBike = bike.mapPartitionsWithIndex(filterBike).cache()
-	gBike.take(5)
-	
-	bike.filter(lambda x: 'Greenwich Ave & 8 Ave' in x).first()
-	
-	gLoc = (40.73901691, -74.00263761)
-	
-	import pyproj
-	
-	def filterTaxi(pId, lines):
-	    if pId==0:
-		next(lines)
-	    import csv
-	    import pyproj
-	    proj = pyproj.Proj(init="epsg:2263", preserve_units=True)
-	    gLoc = proj(-74.00263761, 40.73901691)
-	    sqm = 1320**2
-	    for row in csv.reader(lines):
-		try:
-		    dropoff = proj(float(row[5]), float(row[4]))
-		except:
-		    continue
-		sDistance = (dropoff[0]-gLoc[0])**2 + (dropoff[1]-gLoc[1])**2
-		if sDistance<sqm:
-		    yield row[1][:19]
-		    
-	gTaxi = taxi.mapPartitionsWithIndex(filterTaxi).cache()
-	gTaxi.take(5)
-	
-	lBike = gBike.collect()
-	lTaxi = gTaxi.collect()
-	
-	import datetime
-	count = 0
-	for b in lBike:
-	    bt = datetime.datetime.strptime(b,'%Y-%m-%d %H:%M:%S')
-	    for t in lTaxi:
-		tt = datetime.datetime.strptime(t,'%Y-%m-%d %H:%M:%S')
-		diff = (bt-tt).total_seconds()
-		if diff>0 and diff<600:
-		    count+=1
-		    break
-	sCount = str(count)
-	file = open("Lab10Solution.txt","w")
-	file.write(sCount)
-	file.close()
+def filterTaxi(pId, lines):
+    if pId == 0:
+        next(lines)
+    
+    import csv
+    import pyproj
+    proj = pyproj.Proj(init="epsg:2263", preserve_units=True)  
+    gLoc = proj(-74.00263761, 40.73901691)
+    sqm = 1320 ** 2 
+    for row in csv.reader(lines):
+        try:
+            dropoff = proj(float(row[5]), float(row[4]))
+        except:
+            continue
+        distance = (dropoff[0]-gLoc[0]) ** 2 + (dropoff[1]-gLoc[1]) ** 2
+        if distance < sqm:
+            yield row[1][:19]
+
+def findTrips(_, records):
+    import datetime
+    lastTaxiTime = None
+    for dt, event in records:
+        t = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
+        if event == 1:
+            if lastTaxiTime != None:
+                if (t - lastTaxiTime).total_seconds() < 600:
+                    yield(dt,event)
+        else:
+            lastTaxiTime = t
+
+if __name__ == '__main__':
+    sc = SparkContext()
+    taxi = sc.textFile('/data/share/bdm/yellow.csv.gz')
+    bike = sc.textFile('/data/share/bdm/citibike.csv')
+
+    gBike = bike.mapPartitionsWithIndex(filterBike).cache()
+    gTaxi = taxi.mapPartitionsWithIndex(filterTaxi).cache()
+
+    gAll = (gTaxi.map(lambda x: (x, 0)) + gBike.map(lambda x: (x, 1)))
+
+    print(gAll.sortByKey().mapPartitionsWithIndex(findTrips).count())
